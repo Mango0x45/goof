a0a631e6fe3ab88f2b69abe799d2793c87c4a093
The Depressed Milkman <moi@tdmm.eu>
2022-09-27 09:51:36 +0200

    add EditorConfig with 80-col line length

--- inc/mem.h
+++ inc/mem.h
@@ -28,34 +28,35 @@ typedef struct {
 /**
  * Copy bytes from one slice of memory to another.
  * 
- * Note that this function can be used even if the two memory slices overlap -- it will
- * account for the overlap and ensure that data is copied correctly.
+ * Note that this function can be used even if the two memory slices overlap --
+ * it will account for the overlap and ensure that data is copied correctly.
  */
 void mem_copy(mem_s src, mem_s dst);
 
 /**
  * Copy bytes between two disjoint slices of memory.
  * 
- * This is functionally equivalent to mem_copy(), but takes advantage of the fact that the
- * two slices of memory are known to be disjoint (i.e. do not overlap at all).  It is the
- * caller's responsibility to ensure this invarint upholds -- if it is not guaranteed to
- * hold, mem_copy() should be used instead.
+ * This is functionally equivalent to mem_copy(), but takes advantage of the
+ * fact that the two slices of memory are known to be disjoint (i.e. do not
+ * overlap at all).  It is the caller's responsibility to ensure this invarint
+ * upholds -- if it is not guaranteed to hold, mem_copy() should be used
+ * instead.
  */
 void mem_copy_dj(mem_s src, mem_s dst);
 
 /**
  * Compare two slices of memory for equality.
  * 
- * This function returns true if and only if the two slices contain exactly the same byte
- * values, in the same order.  It is the opposite of mem_isne().
+ * This function returns true if and only if the two slices contain exactly the
+ * same byte values, in the same order.  It is the opposite of mem_isne().
  */
 bool mem_iseq(mem_s lhs, mem_s rhs);
 
 /**
  * Compare two slices of memory for inequality.
  * 
- * This function returns false if and only if the two slices contain exactly the same byte
- * values, in the same order.  It is the opposite of mem_iseq().
+ * This function returns false if and only if the two slices contain exactly the
+ * same byte values, in the same order.  It is the opposite of mem_iseq().
  */
 bool mem_isne(mem_s lhs, mem_s rhs);
 
