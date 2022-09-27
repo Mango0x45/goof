95b2d8d1a5aa550a58c183d29059befbabe442ff
The Depressed Milkman <moi@tdmm.eu>
2022-09-27 08:14:23 +0200

    add mem_copy_dj()
    mem_copy() is modified to guarantee support for overlapping slices, and
    mem_copy_dj() is provided for when slices are guaranteed not to overlap.

--- inc/mem.h
+++ inc/mem.h
@@ -26,7 +26,20 @@ typedef struct {
 
 /**
  * Copy bytes from one slice of memory to another.
+ * 
+ * Note that this function can be used even if the two memory slices overlap -- it will
+ * account for the overlap and ensure that data is copied correctly.
  */
 void mem_copy(mem_s src, mem_s dst);
+
+/**
+ * Copy bytes between two disjoint slices of memory.
+ * 
+ * This is functionally equivalent to mem_copy(), but takes advantage of the fact that the
+ * two slices of memory are known to be disjoint (i.e. do not overlap at all).  It is the
+ * caller's responsibility to ensure this invarint upholds -- if it is not guaranteed to
+ * hold, mem_copy() should be used instead.
+ */
+void mem_copy_dj(mem_s src, mem_s dst);
 
 #endif // #ifndef GOOF_MEM_H
--- src/mem/copy_dj.c
+++ src/mem/copy_dj.c
@@ -2,7 +2,7 @@
 
 #include <string.h>
 
-void mem_copy(mem_s src, mem_s dst) {
+void mem_copy_dj(mem_s src, mem_s dst) {
 	// TODO: Ensure that src and dst have the same length.
-	memmove(dst.ptr, src.ptr, src.len);
+	memcpy(dst.ptr, src.ptr, src.len);
 }
