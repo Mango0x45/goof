b7ec92329255eaf0f0e2640fc75b76be80f194ba
Thomas Voss <mail@thomasvoss.com>
2022-10-04 17:14:35 +0200

    add types.h
    This commit adds a types.h header with some convenient typedefs.  Being able to
    write the mem_* functions with byte instead of unsigned char will make the code
    more descriptive, and names like uint32_t are just annoying.

---
+++ inc/types.h
@@ -0,0 +1,67 @@
+/**
+ * ============================
+ * goof/types: type definitions
+ * ============================
+ */
+
+#ifndef GOOF_TYPES_H
+#define GOOF_TYPES_H
+
+#include <stdint.h>
+
+/**
+ * A byte.
+ */
+typedef unsigned char byte;
+
+/**
+ * A signed 8 bit integer.
+ */
+typedef int8_t i8;
+
+/**
+ * A signed 16 bit integer.
+ */
+typedef int16_t i16;
+
+/**
+ * A signed 32 bit integer.
+ */
+typedef int32_t i32;
+
+/**
+ * A signed 64 bit integer.
+ */
+typedef int64_t i64;
+
+/**
+ * The largest possible signed integer.
+ */
+typedef intmax_t imax;
+
+/**
+ * An unsigned 8 bit integer.
+ */
+typedef uint8_t u8;
+
+/**
+ * An unsigned 16 bit integer.
+ */
+typedef uint16_t u16;
+
+/**
+ * An unsigned 32 bit integer.
+ */
+typedef uint32_t u32;
+
+/**
+ * An unsigned 64 bit integer.
+ */
+typedef uint64_t u64;
+
+/**
+ * The largest possible unsigned integer.
+ */
+typedef uintmax_t umax;
+
+#endif // #ifndef GOOF_TYPES_H
