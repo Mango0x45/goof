85d034f113de08a29e71b8e9ffe2d188abff62ac
Thomas Voss <mail@thomasvoss.com>
2022-10-10 16:33:15 +0200

    [types] Properly support pre-C99/pre-C23/C23 bools
    This commits changes include:
     - Using a typedef to set bool to _Bool instead of a #define
     - Defining _Bool for pre-C99
     - Doing nothing for C23, which adds bool, true, and false to the
       language.

--- inc/types.h
+++ inc/types.h
@@ -11,9 +11,22 @@
 #ifndef GOOF_TYPES_H
 #define GOOF_TYPES_H
 
-#define NULL  ((void*) 0)
-#define bool  _Bool
-#define true  ((_Bool) 1)
-#define false ((_Bool) 0)
+#define NULL ((void *) 0)
+
+// C23 introduces `bool`, `true`, and `false` as keywords.
+#if __STDC_VERSION__ < 202000
+	// C99 introduces the _Bool type.
+	#if __STDC_VERSION__ >= 199901
+		#define true  ((_Bool) 1)
+		#define false ((_Bool) 0)
+	#else
+		typedef enum {
+			false = 0,
+			true  = 1
+		} _Bool;
+	#endif
+
+	typedef _Bool bool;
+#endif
 
 #endif // #ifndef GOOF_TYPES_H
